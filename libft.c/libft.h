4148ded0eceec2dcc401b6ef46c9707b2fbaa288
Orion DOOMS <odooms@c5r1s10.wethinkcode.co.za>
2019-05-24 12:42:32 +0200

    libft.c

--- libft.c/libft.h
+++ libft.c/libft.h
@@ -6,13 +6,19 @@
 /*   By: odooms <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 14:22:16 by odooms            #+#    #+#             */
-/*   Updated: 2019/05/21 14:25:26 by odooms           ###   ########.fr       */
+/*   Updated: 2019/05/24 12:21:04 by odooms           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _LIBFT_H_
-#define _LIBFT_H_
+#ifndef LIBFT_H_
+#define LIBFT_H_
+#include <sting.h>
 
-char* reverse(char *string); 
+int	ft_tolower(int a);
+int	ft_isalnum(int a);
+int	ft_isascii(int a);
+int ft_isdigit(int a);
+int ft_isalpha(int a);
+int ft_toupper(int a);
 
-endif
+#endif
