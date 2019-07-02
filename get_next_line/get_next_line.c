f08576bdee5df86809bb312c98884fce7da6f211
Orion DOOMS <odooms@c5r1s10.wethinkcode.co.za>
2019-07-02 16:49:05 +0200

    2july

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,35 +6,52 @@
 /*   By: odooms <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 14:55:01 by odooms            #+#    #+#             */
-/*   Updated: 2019/06/29 14:53:50 by odooms           ###   ########.fr       */
+/*   Updated: 2019/07/02 16:48:05 by odooms           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <sys/types.h>
 #include "get_next_line.h"
-#define BUF_SIZE 34
+#include <stdio.h>
+/*#define  BUFF_SIZE 30*/
 
-char	*readline(char *str, int fd)
+
+char	*readline(int fd, char *str)
 {
 	char	buff[BUFF_SIZE + 1];
 	int		ret;
-	while(ret)
+
+	while (ret)
 	{
 		ret = read(fd, buff, BUFF_SIZE);
-		buff[ret] = '\0';
-		str = ft_strjoin(str, & buff[ret]);
+	   	buff[ret] = '\0';
+		str	= ft_strjoin(str, buff);
 	}
-	return(str);
+	return (str);
 }
+
+int	get_next_line(int const fd, char **line)
+{
+	char	buff[BUFF_SIZE + 1];
+	int		ret;
+	char 	*temp;
+
+	if (fd <= 0 || line ==  NULL) 
+		return (-1);
+	return (0);
+}
+
 int main()
 {
-	int fd;
-	int ret;
-	char buf;
-
-	fd = open("test", O_RDONLY);
-	ft_putnbr(ret);
-	ft_putstr(&buf);
+	int		fd;
+	int		nbr;
+	char	*tem;
+	char	*s1;
+	fd = open("test2", O_RDONLY);
+	nbr = get_next_line(fd, &tem);
+	ft_putstr(s1 = readline(fd, s1));	
+	ft_putnbr(nbr);
+	close(fd);
 	return (0);
-
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: odooms <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 14:37:39 by odooms            #+#    #+#             */
-/*   Updated: 2019/06/29 10:03:34 by odooms           ###   ########.fr       */
+/*   Updated: 2019/07/02 16:40:47 by odooms           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 8
+# define BUFF_SIZE 16
 # include <unistd.h>
 # include <stdlib.h>
 
