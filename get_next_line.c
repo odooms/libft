a34a60f46b5cf34c37bef6809659c2c3d86513b8
Orion DOOMS <odooms@c5r4s10.wethinkcode.co.za>
2019-07-20 15:03:08 +0200

    20July

--- get_next_line.c
+++ get_next_line.c
@@ -5,56 +5,77 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: odooms <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/26 14:55:01 by odooms            #+#    #+#             */
-/*   Updated: 2019/07/09 13:24:29 by odooms           ###   ########.fr       */
+/*   Created: 2019/07/12 21:49:40 by odooms            #+#    #+#             */
+/*   Updated: 2019/07/20 14:59:20 by odooms           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*readline(char *temp, int fd)
+static	int			readline(char **temp, int fd)
 {
-	char			buff[BUFF_SIZE + 1];
+	char			temp2[BUFF_SIZE + 1];
 	int				ret;
-	char			*s;
+	char			*line;
 
-	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	if (*temp == NULL)
+		*temp = ft_strnew(0);
+	while (!ft_strchr(*temp, '\n') && (ret = read(fd, &temp2, BUFF_SIZE)) > 0)
 	{
-		if (temp == NULL)
-			temp = ft_strnew(1);
-		buff[ret] = '\0';
-		s = temp;
-		temp = ft_strjoin(temp, buff);
-		if (ft_strchr(temp, '\n') != NULL)
-			break ;
+		temp2[ret] = '\0';
+		line = ft_strjoin(*temp, temp2);
+		free(*temp);
+		*temp = NULL;
+		*temp = ft_strdup(line);
+		free(line);
+		line = NULL;
 	}
-	if (*s != 0)
-		free(s);
-	return (temp);
+	return (ret);
 }
 
-int			get_next_line(int const fd, char **line)
+static	int			saveline(char **temp, char **line)
+{
+	int				i;
+	char			*w;
+	char			*str;
+
+	str = *temp;
+	i = 0;
+	while (*str != '\n' && *str != '\0')
+	{
+		i++;
+		str++;
+	}
+	w = *temp;
+	if (*str == '\n')
+	{
+		*line = ft_strsub(*temp, 0, i);
+		*temp = ft_strdup(w + i + 1);
+	}
+	else if (*str == '\0')
+	{
+		*line = ft_strdup(*temp);
+		*temp = ft_strdup(w + 1);
+		free(*temp);
+	}
+	free(w);
+	return (1);
+}
+
+int					get_next_line(int const fd, char **line)
 {
 	static char		*temp;
 	int				i;
-	char			*w;
+	int				x;
 
-	if (fd < 0 || !line)
-		return (-1);
-	i = 0;
-	temp = readline(temp, fd);
-	if (fd > 0)
+	if (fd < 0 || line == NULL ||
+			!(*line = ft_strnew(BUFF_SIZE + 1)) ||
+			(i = readline(&temp, fd)) < 0)
 	{
-		while (temp[i] != '\0' && temp[i] != '\n')
-		{
-			i++;
-		}
-		*line = ft_strsub(temp, 0, i);
-		w = ft_strdup(temp + i + 1);
-		free(temp);
-		temp = w;
-		if (ft_strlen(temp) > 0)
-			return (1);
+		return (-1);
 	}
-	return (0);
+	if (i == 0 && temp[0] == '\0')
+		return (0);
+	x = saveline(&temp, line);
+	return (x);
 }
